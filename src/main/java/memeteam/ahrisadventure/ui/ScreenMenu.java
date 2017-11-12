6221bf62aa51c344d54ff4e8c3221eb786987802
braynstorm <glav0r3zzz4@gmail.com>
2017-11-12 10:36:02 +0200

    Renamed Screen -> ScreenMenu and moved it in a new package.

---
+++ src/main/java/memeteam/ahrisadventure/ui/ScreenMenu.java
@@ -0,0 +1,45 @@
+package memeteam.ahrisadventure.ui;
+
+import com.badlogic.gdx.Screen;
+
+public class ScreenMenu implements Screen {
+	
+	
+	@Override
+	public void show() {
+		/*
+		TODO 1. Add visual elements
+		TODO 2. Add controls and hotkeys
+		 */
+	}
+	
+	@Override
+	public void render(float delta) {
+	
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+	
+	}
+	
+	@Override
+	public void pause() {
+	
+	}
+	
+	@Override
+	public void resume() {
+	
+	}
+	
+	@Override
+	public void hide() {
+	
+	}
+	
+	@Override
+	public void dispose() {
+	
+	}
+}
